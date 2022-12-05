e39183226f0db01315aa1c702d7b2a45eee75d7b
QingyaoFan <qyfan.china@gmail.com>
2022-12-05 01:13:01 -0500

    take an url check function from stackOverflow for testing

--- src/utils.js
+++ src/utils.js
@@ -228,7 +228,13 @@ function checkStringArray(arr, varName) {
 }
 
 function checkUrl(url) {
-
+    var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+    if (!!pattern.test(url)) throw "Invalid URL"
     return url;
 }
 
